dc67e2bbed9fe2764200cfc0f2b20bc0739c6fa8
imfloopa <0512025073@alumnos.unp.edu.pe>
2026-06-15 19:03:16 -0500

    creacion de obtener y establecer

--- src/PerfilMedico.java
+++ src/PerfilMedico.java
@@ -1,2 +1,35 @@
+
+import java.time.LocalDate;
 public class PerfilMedico {
-}
+    String nombre;
+    String apellido;
+    LocalDate fechaNacimiento;
+    double altura;
+    double peso;
+
+    public PerfilMedico(String nombre, String apellido, LocalDate fechaNacimiento, double altura, double peso) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.altura = altura;
+        this.peso = peso;
+        }
+    public String obtenerNombre() {
+        return nombre;
+    }
+    public String obtenerApellido() {
+        return apellido;
+    }
+    public LocalDate obtenerFechaNacimiento() {
+        return fechaNacimiento; 
+    }
+    public double obtenerAltura() {
+        return altura;
+    }public double obtenerPeso() {
+        return peso;
+    }
+    public double calcularIMC() { 
+        return peso / (altura * altura);
+    }
+
+}   
