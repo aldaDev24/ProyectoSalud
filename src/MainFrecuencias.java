7fa6ac9a4d99e460fa53bff5cb9d9edf82264e01
AldaDev <alddev2007@gmail.com>
2026-06-15 22:26:30 -0500

    Esqueleto de la clase MainFrecuencias

--- src/MainFrecuencias.java
+++ src/MainFrecuencias.java
@@ -1,5 +1,23 @@
+import java.time.DateTimeException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class MainFrecuencias {
     public static void main(String[] args) {
 
+        Scanner scanner =  new Scanner(System.in);
+
+        try{
+
+            // Leer Datos
+
+            // Crear Objetos
+
+            // Mostrar Resultados
+
+        }catch(InputMismatchException e){
+
+        }
+        scanner.close();
     }
 }
