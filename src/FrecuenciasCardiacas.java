c2b58c3589b2b0c03a44f5ca954612046e2692cb
AldaDev <alddev2007@gmail.com>
2026-06-15 21:15:17 -0500

    Creacion del esqueleto de FrecuenciasCardiacas

--- src/FrecuenciasCardiacas.java
+++ src/FrecuenciasCardiacas.java
@@ -1,2 +1,22 @@
 public class FrecuenciasCardiacas {
+
+    // Atributos
+    private String nombre;
+    private String apellido;
+    private int diaNacimiento;
+    private int mesNacimiento;
+    private int anioNacimiento;
+
+    public FrecuenciasCardiacas(String nombre,
+                                String apellido,
+                                int diaNacimiento,
+                                int mesNacimiento,
+                                int anioNacimiento) {
+
+    }
+
+    // Metodos para Obtener y Establecer
+
+    //Metodos de calculo
+
 }
